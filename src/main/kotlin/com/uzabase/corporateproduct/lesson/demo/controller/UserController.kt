cee81cf391fabfabd756e55081339472789aefe6
Ittetsu Sato <ittetsusato@gmail.com>
2025-02-03 09:38:15 +0900

    lint

--- src/main/kotlin/com/uzabase/corporateproduct/lesson/demo/controller/UserController.kt
+++ src/main/kotlin/com/uzabase/corporateproduct/lesson/demo/controller/UserController.kt
@@ -10,12 +10,12 @@ data class User(val id: UUID, val name: String)
 @RestController
 class UserController {
     @GetMapping("/users/{userId}/workHistories/{workHistoryId}")
-    fun getUserById(@PathVariable pathVarsMap: Map<String, String>, @RequestParam("year") year: String, @RequestParam("sort", required = false, defaultValue = "asc") sort: String?): ResponseEntity<User> {
-        val userId = pathVarsMap["userId"]
-        val workHistoryId = pathVarsMap["workHistoryId"]
-        if (userId == null || workHistoryId == null) {
-            return ResponseEntity.badRequest().build()
-        }
+    fun getUserById(
+        @PathVariable userId: String,
+        @PathVariable workHistoryId: String,
+        @RequestParam("year") year: String,
+        @RequestParam("sort", required = false, defaultValue = "asc") sort: String?
+    ): ResponseEntity<User> {
         val logger = LoggerFactory.getLogger(javaClass)
         logger.debug("userId: {}, workHistoryId: {}, year: {}, sort: {}", userId, workHistoryId, year, sort)
         return ResponseEntity.accepted().build()
